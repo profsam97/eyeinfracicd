a848b8be4a5f84088ad2b3962c1aa27d4ace88f8
profsam97 <proftoby4real@hotmail.com>
2023-11-08 16:17:54 +0100

    updated  baseurl

--- client/src/Helpers/BaseUrl.ts
+++ client/src/Helpers/BaseUrl.ts
@@ -5,6 +5,6 @@ const production : string = 'http://eye.vprofile.cloudradar.online';
 const baseurl : string =   production;
 
 
-export const server = process.env.NEXT_PUBLIC_SERVER || 'http://192.168.10:5000' 
+export const server = process.env.NEXT_PUBLIC_SERVER || 'http://192.168.33:5000' 
 
 export default baseurl;
